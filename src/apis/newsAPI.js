1c33e9bea6787d464e440f7e429bdf95a6bfb151
Ian Lyons <ilyons@gmail.com>
2018-03-31 14:57:00 -0700

    more api stuff

--- src/apis/newsAPI.js
+++ src/apis/newsAPI.js
@@ -1,6 +1,36 @@
-export const get = () => {
-  const url =
-    'https://newsapi.org/v2/top-headlines?country=us&apiKey=377fd71cddec43ef803e2daf782744de';
-  const req = new Request(url);
+import * as qs from 'querystring';
+
+const baseUrl = 'https://newsapi.org/v2';
+
+/**
+ *
+ * @param {string} url - The endpoint name. For the `everything` endpoint, this would be `/everything`
+ * @param {*} params - Query parameters.
+ * @param {*} opts - Options or overrides to be passed to the request object.
+ */
+export const BaseRequest = (url, params, opts = {}) => {
+  // Set up authentication so consumers of this don't need to worry about it. Can be overridden
+  // by the passed-in params.
+  const allOpts = Object.assign(
+    {
+      headers: {
+        Authorization: window.NEWS_API_KEY,
+      },
+    },
+    opts
+  );
+  return new Request(`${baseUrl}${url}?${qs.stringify(params)}`, allOpts);
+};
+
+export const get = (params = {}) => {
+  const allParams = Object.assign(
+    {
+      pageSize: 100,
+      page: 1,
+      country: 'us',
+    },
+    params
+  );
+  const req = BaseRequest('/everything', allParams);
   return fetch(req).then(res => res.json());
 };

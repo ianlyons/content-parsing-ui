9cde6a33d1d6228e9ab11726d6a9c178da979278
Ian Lyons <ilyons@gmail.com>
2018-04-22 11:49:02 -0700

    fix all the slider styling

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -6,11 +6,11 @@ export default class Slider extends React.Component {
   render() {
     const { onChange, labelLeft, labelRight, id, color } = this.props;
     return (
-      <div className={`Slider Slider--${color}`}>
+      <div className="Slider">
         <label className="Slider-label" htmlFor={id}>
           {labelLeft}
         </label>
-        <input className="Slider-input" min={0} max={10} step={1} id={id} onChange={onChange} type="range" />
+        <input className={`Slider-input Slider-input--${color}`} min={0} max={100} step={1} id={id} onChange={onChange} type="range" />
         <label className="Slider-label" htmlFor={id}>
           {labelRight}
         </label>
@@ -26,3 +26,4 @@ Slider.propTypes = {
   id: PropTypes.string,
   color: PropTypes.string, // supported are `blue` and `green`
 };
+

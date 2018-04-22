b57ec2b350845990bbf8c9505ca47a7928669b13
Ian Lyons <ilyons@gmail.com>
2018-04-22 12:09:21 -0700

    rough wire-up of sliders, update article styling

--- src/App.js
+++ src/App.js
@@ -3,12 +3,56 @@ import React, { Component } from 'react';
 // import PublisherContainer from './containers/PublisherContainer';
 // import PersonalizationContainer from './containers/PersonalizationContainer';
 import Slider from './components/Slider/Slider';
+import Article from './components/Article/Article';
 import './App.css';
 
 class App extends Component {
   state = {
-    view: 'personalization',
+    personalization: 50,
+    publisher: 50,
+    loginStatus: null,
   };
+
+  updateSliderValue = e => {
+    const { id: inputId, value } = e.target;
+    this.setState({
+      [inputId]: parseInt(value, 10),
+    });
+  };
+
+  getCurrentArticles = () => {
+    return [
+      {
+        headline: 'Something happened this morning!!111',
+        source: 'Washington Post',
+        url: 'https://yahoo.com',
+      },
+      {
+        headline: 'Something happened this morning!!111',
+        source: 'Washington Post',
+        url: 'https://yahoo.com',
+      },
+      {
+        headline: 'Something happened this morning!!111',
+        source: 'Washington Post',
+        url: 'https://yahoo.com',
+      },
+      {
+        headline: 'Something happened this morning!!111',
+        source: 'Washington Post',
+        url: 'https://yahoo.com',
+      },
+      {
+        headline: 'Something happened this morning!!111',
+        source: 'Washington Post',
+        url: 'https://yahoo.com',
+      },
+    ];
+  };
+
+  componentDidUpdate() {
+    console.log(this.state);
+  }
 
   render() {
     return (
@@ -17,27 +61,31 @@ class App extends Component {
           <h1 className="App-title">A News Tool</h1>
           <h2 className="App-subtitle">Adjust the sliders, tune your news.</h2>
           <h2 className="App-subtitle">
-            Powered by{' '}
+            Powered by&nbsp;
             <a href="https://newsapi.org" rel="noopener noreferrer" target="_blank">
               NewsAPI.org
             </a>
           </h2>
         </header>
         <div className="App-body">
+          <div className="App-articleWrapper">
+            {this.getCurrentArticles().map((article, i) => <Article {...article} />)}
+          </div>
+
           <div className="App-sliderContainer">
             <Slider
               color="blue"
               labelLeft="Untraditional publisher"
               labelRight="Established publisher"
               id="publisher"
-              onChange={console.log}
+              onChange={this.updateSliderValue}
             />
             <Slider
               color="green"
               labelLeft="Personalization algorithm"
               labelRight="Human editors"
               id="personalization"
-              onChange={console.log}
+              onChange={this.updateSliderValue}
             />
           </div>
         </div>
--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,30 +1,19 @@
 import * as React from 'react';
-import * as _ from 'lodash';
 import * as PropTypes from 'prop-types';
 import './Article.css';
 
-const normalizeUrl = (url) => {
-  if (_.startsWith(url, 'http')) return url;
-  return `https://${url}`;
-}
-
-const getStyle = (url) => {
-if (!url) return {};
-  return { backgroundImage: `url(${normalizeUrl(url)})`};
-}
-
 export default class Article extends React.Component {
   render() {
-    const { imgSrc, headline, url, source } = this.props;
+    const { headline, url, source } = this.props;
 
     return (
       <article className="Article">
-        <div className="Article-contentWrapper" style={getStyle(imgSrc)}>
+        <header className="Article-header">{source}</header>
+        <div className="Article-contentWrapper">
           <a className="Article-link" href={url} target="_blank">
             <h4 className="Article-headline">{headline}&nbsp;</h4>
           </a>
         </div>
-        <footer className="Article-footer">{source}</footer>
       </article>
     );
   }
